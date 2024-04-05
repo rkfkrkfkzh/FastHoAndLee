030347753ca6d8f65d067f6cbc293079d16d925d
imhyojin <1004dlagywls@naver.com>
2024-04-05 13:58:30 +0900

    springboot vue 통합 설정

--- src/main/java/com/example/fasthoandlee/config/WebConfig.java
+++ src/main/java/com/example/fasthoandlee/config/WebConfig.java
@@ -1,8 +1,14 @@
 package com.example.fasthoandlee.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,5 +18,23 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 허용할 엔드포인트 패턴을 지정
                 .allowedOrigins("*") // 허용할 Origin을 명시
                 .allowedMethods("GET", "POST", "PUT", "DELETE"); // 허용할 HTTP 메서드를 지정
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**/*")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+
+
     }
 }
