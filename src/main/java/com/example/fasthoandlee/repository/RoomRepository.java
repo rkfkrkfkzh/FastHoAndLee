b24f5d1ba7a7ef24a13c910fe9ebb37a5f45b86e
imhyojin <1004dlagywls@naver.com>
2024-07-05 10:59:19 +0900

    빌드 배포 전 환경변수 설정 .env 생성

--- src/main/java/com/example/fasthoandlee/repository/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.fasthoandlee.repository;
-
-import com.example.fasthoandlee.domain.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
